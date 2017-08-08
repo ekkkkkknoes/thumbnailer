fc26ba578455ba6d10c63f31eea4231e19189f12
Sönke Lambert <soelam@live.de>
2017-08-08 13:23:45 +0200

    Adds download url

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 
 setup(
     name="thumbnailer",
-    version="1.0",
+    version="1.0.1",
     description="Module to turn images into thumbnails",
     author="Sönke Lambert",
     author_email="soelam@live.de",
     url="https://github.com/ekkkkkknoes/thumbnailer",
+    download_url="https://github.com/{username}/{module_name}/archive/{tag}.tar.gz",
     scripts=["bin/tbnailer.py"],
     packages=["thumbnailer"]
 )
